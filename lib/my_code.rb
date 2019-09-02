e41d54f3a6aa39ae822bd6f7616294bfc11f3a51
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-02 21:07:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,3 +11,12 @@ end
 final_array
  end
  
+ def map_to_no_change(array)
+  final_array = []
+  counter = 0
+  while counter < array.size do
+    final_array.push(array[counter] )
+    counter += 1
+end
+final_array
+ end
